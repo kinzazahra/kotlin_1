3189a1f8920db2a3d24c7ba976649531a2f95baf
Kinza Zahra <yourgithubemail@example.com>
2026-05-21 20:04:00 +0530

    First Kotlin project

---
+++ Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    println("===== Student Grade Calculator =====")
+
+    val name = "Kinza"
+    val marks = 85
+
+    if (marks >= 40) {
+        println("$name passed 🎉")
+    } else {
+        println("$name failed 😢")
+    }
+}
